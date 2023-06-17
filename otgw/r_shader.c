13e8df5f2e3298842685f423ea3b129f8b542aa5
James Hicks <97328702+Fireinthesky1@users.noreply.github.com>
2023-06-16 17:38:28 -0700

    Fixed shader compilation bug
    -The problem was I was passing in the source code to glCompileShader instead of passing in the ID

--- otgw/r_shader.c
+++ otgw/r_shader.c
@@ -2,17 +2,17 @@
 
 #include "r_shader.h"
 
-void s_bind_shader_source(struct shader* shader)
+void s_bind_shader_source(struct shader* i_shader)
 {
-    glShaderSource(shader->id, 1, &(shader->source_code), NULL);
+    glShaderSource(i_shader->id, 1, &(i_shader->source_code), NULL);
 }
 
-void s_compile_shader(struct shader* shader)
+void s_compile_shader(struct shader* i_shader)
 {
     char* shader_type_as_string;
 
     // TODO(JAMES): come back and fill out for all shader types
-    if (shader->type == GL_VERTEX_SHADER)
+    if (i_shader->type == GL_VERTEX_SHADER)
     {
         shader_type_as_string = "VERTEX SHADER";
     }
@@ -21,16 +21,16 @@ void s_compile_shader(struct shader* shader)
         shader_type_as_string = "FRAGMENT SHADER";
     }
 
-    glCompileShader(shader->source_code);
+    glCompileShader(i_shader->id);
 
     GLint vertex_compiled;
-    glGetShaderiv(shader->id, GL_COMPILE_STATUS, &vertex_compiled);
+    glGetShaderiv(i_shader->id, GL_COMPILE_STATUS, &vertex_compiled);
     if (vertex_compiled != GL_TRUE)
     {
         GLsizei log_length = 0;
         GLchar message[1024];
-        glGetShaderInfoLog(shader->id, 1024, &log_length, message);
+        glGetShaderInfoLog(i_shader->id, 1024, &log_length, message);
         printf
-        ("ERROR::%s COMPILATION FAILED:: %s",shader_type_as_string,message);
+        ("ERROR::%s COMPILATION FAILED::%s\n\n",shader_type_as_string,message);
     }
 }
